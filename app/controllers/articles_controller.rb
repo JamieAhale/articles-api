6cf5c58673480c55f8bee5d276ec94fc051bf75d
Jamie Ahale <jamie.w.ahale@gmail.com>
2024-06-11 13:33:21 +1000

    Initial commit'

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
+  def index
+    @articles = Article.all 
+    render json: @articles
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @article
+  end
+
+  def edit
+    render json: @article
+  end
+
+  def update
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
